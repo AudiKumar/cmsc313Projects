99472081bbb69c2401950435480042888f8daabe
Aditya Kumar <adi6@umbc.edu>
2020-04-15 18:49:16 -0400

    Project 3: First Commit

---
+++ Project3/AdityaKrishna_YerraguntlaKumar_split.asm
@@ -0,0 +1,7 @@
+; Name: Aditya Krishna Yerraguntla Kumar
+; File: AdityaKrishna_YerraguntlaKumar_split.asm
+; Date: April 15 2020
+; Description:  This program will start first by asking the user for a 15 character
+;               string. The program will then display said string and then split
+;               the string at that location and put the second part of that string 
+;               at the front.
