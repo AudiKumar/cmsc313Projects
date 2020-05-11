89d4830467d49eda8ce8f9cc47264a80dc8e6201
Aditya Kumar <adi6@umbc.edu>
2020-05-11 12:22:21 -0400

    Proj 4: Main Read Option (Not Tested Yet)

--- Project4/main.asm
+++ Project4/main.asm
@@ -1,6 +1,3 @@
-adi6@linux5:~/313/projects/proj4$ ls
-jump.asm  main  main.asm  main.o  makefileTemplate  split.asm  square.c
-adi6@linux5:~/313/projects/proj4$ cat main.asm 
 ; Name: Aditya Krishna Yerraguntla Kumar
 ; File: main.asm
 ; Date: May 1 
@@ -15,23 +12,42 @@ adi6@linux5:~/313/projects/proj4$ cat main.asm
 
 ; lol screw 80 columns
 menuMessage:  db    "Encryption menu options:", 10, "d - display the current message", 10, "r - read new message", 10, "s - split encrypt", 10, "j - jump encrypt", 10, "q - quit program", 10, "enter option letter -> ",0 
-errorMenuMessage: db "Invalid Menu Option, please enter a valid menu option"
+
+
 msg:          db    "This is the original message.", 10
-display:      db    "d"
-read:         db    "r" 
-split:        db    "s"
-jump:         db    "j"
-quit:         db    "q"
-true:         db    "true"
-false:        db    "false"
+msgLen:       equ $ - msg
+
+display:      db    "d", 0
+dLen:         equ $ - display
+
+read:         db    "r", 0
+readLen:      equ $ - read
+ 
+readPrompt:   db    "Enter a new message: ", 0
+RPLen:        equ $ - readPrompt
+errorRead:    db    "Invalid message, keeping the current message.", 10
+erLen:        equ $ - errorRead
+
+
+splitChoice:  db    "s", 0
+sLen:         equ $ - splitChoice
+ 
+jump:         db    "j", 0
+jLen:         equ $ - jump
+
+quit:         db    "q", 0 
+qLen:         equ $ - quit
+
+
 newline:      db    10
-fmt:           db "%s", 0
-;inputfmt:      db "%s", 0, 10 
+fmt:          db "%s", 0
+nullTerminator: db   0
+
 
         section .bss
-choice  resb    2
-string  resb    256 
-
+choice      resb 2
+string      resb 256 
+readInput   resb 256 
 
         section .text
         global main
@@ -41,11 +57,11 @@ string  resb    256
 main:
 
 
-      jmp while
+      jmp copyOgMSGIntoBuffer      
+
 
 
 while:
-
       
       ;this code forces the executable file to go to the newline even if the reserve is the buffer has been used
       mov rax, 1
@@ -75,17 +91,20 @@ while:
 
 
       ;if (choice == display): display the message
-      ;cmp al, [display]      
+      cmp al, [display]      
+      je displayOption      
 
       ;if (choice == read): enter a new option
-      ;cmp al, [read]
+      cmp al, [read]
+      je readOption
+      
 
       ;if (choice == split): start the split subroutine
-      ;cmp al, [split]      
+      cmp al, [splitChoice]      
 
 
       ;if (choice == jump): start the jump split subroutne
-      ;cmp al, [jump]
+      cmp al, [jump]
 
 
       ;if(choice == quit): exit program 
@@ -93,20 +112,144 @@ while:
       JE exit
 
 
-
-      ;outputs only if there is choice selected is not valid
-      push rbp
-      mov rdi, fmt
-      mov rsi, errorMenuMessage
-      mov rax, 0
-      call scanf
-      pop rbp 
-
       ;starts the loop over again
       JNE main
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; loop explained in psedocode       
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; r9 = counter (which starts at 0)
+; r10 = the len of og msg 
+; while ( counter < len(ogMsg) ):
+;     buffer += ogMsg[counter]
+;     counter++
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;copyOgMSGIntoBuffer()  
+;description:   
+;             contains all of the declarations, that will be used in the loop
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+copyOgMSGIntoBuffer:
+      
+      ;r8 ==> counter = 0;
+      mov r8, msg
+      mov r9, 0
+      mov r10, msgLen
+      mov r11, string
+
+      jmp copyLoop
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;copyLoop()  
+;description:   
+;             this is where the looping will happen for putting the og msg into
+;             the string buffer
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+copyLoop:
+
+      ;if counter == msgLen: then start the menu option loop
+      cmp r9, r10     
+      JE while
+
+      ;else put the current char into the buffer
+      
+      ; goes through the og msg and copys it char by char (the char is stored in AL)
+      mov r8, msg 
+      add r8, r9 
+      mov al, [r8]
+      
+      mov [r11], al
+      inc r9
+      inc r11 
+      jmp copyLoop
         
+      
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;displayOption():
+;decription:
+;            subrountine that will display the current message, this will call
+;            printf and go back to the the while loop
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+displayOption:
+      
+      push rbp
+      mov rdi, fmt
+      mov rsi, string
+      mov rax, 0
+      call printf
+      pop rbp
+
+      jmp while
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;readOption():
+;decription:
+;           subrountine that will read in a new input, but before it overwrites
+;           the string buffer the input is validated. Validation means that the
+;           first char is an upper case letter, and the 2nd to last char (b/c
+;           the last char is the newline or enter ascii character)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+readOption: 
+
+      ;outputs the readPrompt
+      push rbp
+      mov rdi, fmt
+      mov rsi, readPrompt
+      mov rax, 0
+      call printf
+      pop rbp
+
+      ; uses scanf to put the user's choice into the readInput
+      push rbp
+      mov rdi, fmt
+      mov rsi, readInput
+      mov rax, 0
+      call scanf
+      pop rbp
+      
+
+      ;stores the begining of the read input (will also be used for looping)
+      mov r8, readInput
+
+      ;stores the first char in the read input into the 1st parameter for the
+      ;validate subrountine. dil = 1 btye version of rsi 
+      mov dil, [readInput]
+      
+      
+      ;jmp validateInput
+      ;setup for getting the last char
+      mov r11, [nullTerminator]
+      jmp getLastChar
+
+getLastChar:
+      ;checks to see if the current index is the null terminator 
+      cmp r8, r11 
+      JE setupValidate
+
+      inc r8
+      jmp getLastChar
 
       
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;setupValidate()  
+;description:   
+;             puts of the the stuff in the proper registers for the validate
+;             subroutine 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+setupValidate:
+
+      ;decrements the pointer of the readInput 
+      dec r8
+      
+      ;stores the last char in the read input into the 2nd parameter for the 
+      ;validate subroutine. sil = 1 byte version of rsi 
+      mov sil, [r8]
+
 
 
 ;exit for the code
---
+++ Project4/validate.asm
@@ -0,0 +1,41 @@
+; Name: Aditya Krishna Yerraguntla Kumar
+; File: validate.asm
+; Date: May 9
+; Description: 
+;              this subrountine will check the first char and last char to check 
+;              to see if the the message is "valid". A message is valid if it 
+;              starts with an uppercase letter and ends with a ., ! or ? 
+; 
+;              here is some helpful values that will be needed for a comparison
+;              in terms of ascii values 
+;                   1.) A = 65, Z = 90
+;                   2.). = 46, ! = 33, ? = 63 
+; 1 should be used for the first char check. 2 is for the last char 
+; check. If it goes back to main then it's a valid input 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; PSEUDOCODE ALGO 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;             
+; rdi = firstChar
+; rsi = lastChar
+;
+; firstCharCheck1: 
+;       if firstChar >= "A": GoTo firstCharCheck2()
+;       else: notValidInput()
+;      
+; firstCharCheck2:        
+;       if firstChar <= "Z": GoTo lastCharCheck1()
+;       else: notValidInput()
+;
+; lastCharCheck1:
+;       if lastChar == ".": GoTo Main()
+;       else: GoTo lastCharCheck2 
+;
+;lastCharCheck2: 
+;       if lastChar == "?": GoTo Main()
+;       else: GoTo lastCharCheck3
+;
+;lastCharCheck3:
+;       if lastChar == "!": GoTo Main()
+;       else: notValidInput
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
